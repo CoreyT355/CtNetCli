9bf79df06cf26fb92880bfa25c9bf5e1644692c8
Corey <corey.a.tess@gmail.com>
2017-01-26 21:21:05 -0500

    Fixing the main blog route

--- src/app/blog/blog.routing.ts
+++ src/app/blog/blog.routing.ts
@@ -8,7 +8,7 @@ import { AuthGuard } from '../auth/auth-guard.service';
 const appRoutes: Routes = [
   { path: 'blog',
     component: BlogComponent,
-    canActivate: [AuthGuard],
+    //canActivate: [AuthGuard],
     children: [
       { path: '', component: BlogComponent }
     ]
