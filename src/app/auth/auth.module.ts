8dce03d0be836db5d9b5e082c20aa8c2fadf3634
Corey <corey.a.tess@gmail.com>
2017-02-26 22:07:01 -0500

    General style updates

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { CovalentCoreModule } from '@covalent/core';
 
 import { authRouting } from './auth.routing';
 import { LoginComponent } from './login.component';
@@ -11,7 +12,8 @@ import { ResetpassComponent } from './resetpass.component';
   imports:      [ 
     authRouting,
     FormsModule,
-    CommonModule
+    CommonModule,
+    CovalentCoreModule
    ],
   declarations: [ 
     SignupComponent,
