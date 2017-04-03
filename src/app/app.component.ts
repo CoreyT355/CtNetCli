3995333a3dbd2eec50585df1c337677754ab95e3
Corey <corey.a.tess@gmail.com>
2017-04-02 21:37:19 -0400

    General Improvements
    Menu uses an array now. Loading page now has site logo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,31 @@ import { AngularFire } from 'angularfire2';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { 
+export class AppComponent {
+  routes: Object[] = [{
+    title: 'Blog',
+    icon: 'fa-rss',
+    route: '/blog'
+  }, {
+    title: 'Projects',
+    icon: 'fa-folder-open-o',
+    route: '/projects'
+  }, {
+    title: 'Portfolio',
+    icon: 'fa-newspaper-o',
+    route: '/portfolio'
+  }, {
+    title: 'About',
+    icon: 'fa-user-o',
+    route: '/about'
+  }];
+
   constructor(private af: AngularFire, private router: Router, private _iconRegistry: MdIconRegistry, private _domSanitizer: DomSanitizer) {
     this._iconRegistry.addSvgIconInNamespace('assets', 'ctnet-logo', this._domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/ctnet-logo.svg'));
   }
 
   logout() {
-        this.af.auth.logout();
-        this.router.navigate(['/'])
-    }
+    this.af.auth.logout();
+    this.router.navigate(['/'])
+  }
 }
